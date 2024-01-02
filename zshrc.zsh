7ba89f3c87c213deb69d6eef0cbb633b5208425a
Jonas <jonas.lovig@gmail.com.com>
2024-01-02 20:45:50 +0100

    add my-zsh setup to git

--- zshrc.zsh
+++ zshrc.zsh
@@ -19,3 +19,8 @@ source "$ZCONFIG_HOME/modules/general.zsh"
 source "$ZCONFIG_HOME/modules/history.zsh"
 source "$ZCONFIG_HOME/modules/completion.zsh"
 source "$ZCONFIG_HOME/modules/plugins.zsh"
+export PATH=/home/jtl/bin:$PATH
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+export PATH=~/bin:$PATH
